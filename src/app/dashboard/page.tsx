832b63cae16359a9bff718413e719bb373943083
unknown <support@prosyncedu.com>
2026-05-13 11:32:43 +0530

    Fixed stale rendering on vercel

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,6 +2,11 @@ import { getCards } from "@/modules/business-card/services/get-cards";
 
 import DashboardClient from "@/modules/business-card/components/DashboardClient";
 
+export const dynamic =
+  "force-dynamic";
+
+export const revalidate = 0;
+
 export default async function DashboardPage() {
   const cards = await getCards();
 
